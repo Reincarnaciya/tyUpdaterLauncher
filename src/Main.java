6824b91ff49a32090e17fe21fbaa2a8a9c89cae3
Reincarnaciya <99612262+Reincarnaciya@users.noreply.github.com>
2022-12-07 19:36:19 +0500

    first push

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
+import Utils.Updater;
+
+import javax.rmi.CORBA.Util;
+import java.awt.*;
+import java.io.Console;
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static String pathToLauncher;
+    public static String pathToLauncherDir;
+
+    public static void main(String[] args) throws IOException {
+        pathToLauncher = args[0];
+        pathToLauncherDir = args[1];
+        deleteFile(pathToLauncher);
+        deleteFile(pathToLauncherDir);
+        if(!new File(pathToLauncher).exists()){
+            Utils.Updater.DownloadUpdate(pathToLauncher);
+        }
+
+    }
+
+    static void deleteFile(String file){
+        new File(file).delete();
     }
 }
---
+++ src/Utils/Updater.java
@@ -0,0 +1,50 @@
+package Utils;
+
+import javax.net.ssl.HttpsURLConnection;
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class Updater {
+    public static boolean downloading = false;
+
+        //http://typro.space/files/launcher/TyLauncher.jar
+
+    public static void DownloadUpdate(String path) {
+            new Thread(() -> {
+                try {
+                    downloading = true;
+                    URL url = new URL("https://typro.space/files/launcher/TyLauncher.jar");
+                    HttpsURLConnection updcon = (HttpsURLConnection) url.openConnection();
+                    System.out.println(updcon);
+                    long cll_web = updcon.getContentLength();
+                    File pcFile = new File(path);
+                    System.err.println(cll_web);
+                    pcFile.createNewFile();
+                    System.err.println(path + File.separator + "TyLauncher.jar");
+                    if ((pcFile.length() != cll_web) && cll_web > 1) {
+                        BufferedInputStream bis = new BufferedInputStream(updcon.getInputStream());
+                        FileOutputStream fw = new FileOutputStream(pcFile);
+                        byte[] by = new byte[1024];
+                        int count = 0;
+                        System.out.println("Скачиваем новый лаунчер..");
+                        while ((count = bis.read(by)) != -1) {
+                            fw.write(by, 0, count);
+                        }
+                        fw.close();
+                        downloading = false;
+                    }
+
+                } catch (IOException e) {
+                    downloading = false;
+                    e.printStackTrace();
+                }
+            }).start();
+    }
+}
+
