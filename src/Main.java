3862b090a425fb4003d0aa28973b8e6776e2800e
Reincarnaciya <99612262+Reincarnaciya@users.noreply.github.com>
2022-12-09 18:47:24 +0500

    Update Main.java
    added self-close and self-delete

--- src/Main.java
+++ src/Main.java
@@ -6,15 +6,23 @@ public class Main {
     public static String pathToLauncher;
     public static String pathToLauncherDir;
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, InterruptedException {
         pathToLauncher = args[0];
         pathToLauncherDir = args[1];
         deleteFile(pathToLauncher);
         deleteFile(pathToLauncherDir);
-        if(!new File(pathToLauncher).exists()){
-            Utils.Updater.DownloadUpdate(pathToLauncher);
-        }
-        Runtime.getRuntime().exec(new String[]{"cmd","/c","start","cmd","/k","java -jar \"" + pathToLauncher + "\""});
+
+        Utils.Updater.DownloadUpdate(pathToLauncher);
+
+        System.err.println("Проверка..");
+        Thread.sleep(600);
+        System.err.println("Запускаю лаунчер..");
+        Runtime.getRuntime().exec("java -jar \"" + pathToLauncher + "\"" + " deleteUpdater");
+        System.err.println("Вы можете закрыть это окно.");
+        Runtime.getRuntime().exec("taskkill /F /IM cmd.exe");
+        Runtime.getRuntime().exit(0);
+        Runtime.getRuntime().halt(0);
+        System.exit(0);
 
     }
 
